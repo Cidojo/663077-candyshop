2ace7275939b9dbf1c88b220e479dc6c05424ec2
Cidojo <cidell@mail.ru>
2018-09-06 15:41:41 +0300

    and DRY my only dream

--- js/goods.js
+++ js/goods.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Part 1. Generate array of card objects
+
+// Data
+
 var CARD_NAME = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие',
   'Паприколу итальяно', 'Нинзя-удар васаби', 'Хитрый баклажан', 'Горчичный вызов', 'Кедровая липучка',
   'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград', 'Сельдерейная душа',
@@ -17,58 +21,68 @@ var CONTENTS = ['молоко', 'сливки', 'вода', 'пищевой кр
   'ароматизатор дуба, идентичный натуральному', 'ароматизатор картофеля', 'лимонная кислота', 'загуститель', 'эмульгатор',
   'консервант: сорбат калия', 'посолочная смесь: соль, нитрит натрия', 'ксилит ', 'карбамид', 'вилларибо', 'виллабаджо'];
 
-function generateNumber(from, to, adjustRound) {
-  var number = from + Math.round(Math.random() * (to - from));
+// get random number from interval
 
-  if (!isNaN(adjustRound) || adjustRound !== undefined) {
-    number += Math.round((number % adjustRound) / adjustRound) * adjustRound - (number % adjustRound);
+function getRandomInt(min, max, round) {
+  var number = min + Math.round(Math.random() * (max - min));
+
+  if (!isNaN(round) || round !== undefined) {
+    number = Math.round((number / round)) * round;
   }
 
   return number;
 }
 
-function generateRating() {
+// generates rating object
+
+function getRating() {
   var myObj = {};
 
-  myObj.value = generateNumber(1, 5);
-  myObj.number = generateNumber(10, 900);
+  myObj.value = getRandomInt(1, 5);
+  myObj.number = getRandomInt(10, 900);
 
   return myObj;
 }
 
-function generateNutrition(contentsList) {
+// generates nutrition_facts object
+
+function getNutrition(contentsList) {
   var myObj = {};
   var myArray = contentsList.slice(0);
-  var tries = generateNumber(1, myArray.length - 1);
+  var tries = getRandomInt(1, myArray.length - 1);
   var contents = [];
 
   for (var i = 0; i < tries; i++) {
-    var randomIndex = generateNumber(0, myArray.length - 1);
+    var randomIndex = getRandomInt(0, myArray.length - 1);
 
     contents.push(myArray[randomIndex]);
     myArray.splice(randomIndex, 1);
   }
 
   myObj.sugar = !!Math.round(Math.random());
-  myObj.energy = generateNumber(70, 500);
+  myObj.energy = getRandomInt(70, 500);
   myObj.contents = contents.join(', ') + '.';
 
   return myObj;
 }
+
+// creates new catalog card
 
 function createCard(name, imgLink) {
   var myObj = {};
 
   myObj.name = name;
   myObj.picture = 'img/cards/' + imgLink;
-  myObj.amount = generateNumber(0, 20);
-  myObj.price = generateNumber(100, 1500, 50);
-  myObj.weight = generateNumber(30, 300);
-  myObj.rating = generateRating();
-  myObj['nutrition_facts'] = generateNutrition(CONTENTS);
+  myObj.amount = getRandomInt(0, 20);
+  myObj.price = getRandomInt(100, 1500, 50);
+  myObj.weight = getRandomInt(30, 300);
+  myObj.rating = getRating();
+  myObj['nutrition_facts'] = getNutrition(CONTENTS);
 
   return myObj;
 }
+
+// build specified amount of catalog cards
 
 function collectCards(quantity) {
   var cardsCollection = [];
@@ -76,8 +90,8 @@ function collectCards(quantity) {
   var clonePictureNames = PICTURE_NAMES.slice(0);
 
   for (var i = 0; i < quantity; i++) {
-    var nameIndex = generateNumber(0, cloneNames.length - 1);
-    var pictureNamesIndex = generateNumber(0, clonePictureNames.length - 1);
+    var nameIndex = getRandomInt(0, cloneNames.length - 1);
+    var pictureNamesIndex = getRandomInt(0, clonePictureNames.length - 1);
 
     cardsCollection.push(createCard(cloneNames[nameIndex], clonePictureNames[pictureNamesIndex]));
     cloneNames.splice(nameIndex, 1);
@@ -88,40 +102,159 @@ function collectCards(quantity) {
 }
 
 var cards = collectCards(26);
-//
-// for (var i = 0; i < CARDS.length; i++) {
-// console.log(CARDS[i].picture)
-// }
 
-document.querySelector('.catalog__cards').classList.remove('catalog__cards--load');
+// Part 2. DOM tree modification
+
 document.querySelector('.catalog__load').classList.add('visually-hidden');
 
-var cardTemplate = document.querySelector('#card').content.querySelector('.catalog__card');
-var catalogNest = document.querySelector('.catalog__cards-wrap');
+var catalogCardsNest = document.querySelector('.catalog__cards');
+catalogCardsNest.classList.remove('catalog__cards--load');
 
-for (var i = 0; i < cards.length; i++) {
-  var newCard = cardTemplate.cloneNode(true);
+// fill textContent property of an owner
 
-  if (cards[i].amount > 5) {
-    newCard.classList.add('card--in-stock');
-  } else if (cards[i].amount >= 1 && cards[i].amount <= 5) {
-    newCard.classList.add('card--little');
-  } else if (cards[i].amount === 0) {
-    newCard.classList.add('card--soon');
-  }
-
-  newCard.querySelector('.card__title').textContent = cards[i].name;
-  newCard.querySelector('.card__img').src = cards[i].picture;
-  newCard.querySelector('.card__price').firstChild.textContent = cards[i].price + ' ';
-  newCard.querySelector('.card__weight').textContent = '/ ' + cards[i].weight + ' Г';
-  newCard.querySelector('.star__count').textContent = cards[i].rating.number;
-
-  if (cards[i].nutrition_facts.sugar === true) {
-    newCard.querySelector('.card__characteristic').textContent = 'Без сахара. ' + cards[i].nutrition_facts.energy + ' ккал';
-  } else {
-    newCard.querySelector('.card__characteristic').textContent = 'Содержит сахар. ' + cards[i].nutrition_facts.energy + ' ккал';
-  }
-  newCard.querySelector('.card__composition-list').textContent = cards[i].nutrition_facts.contents;
-
-  catalogNest.appendChild(newCard);
+function fillTextContent(owner, text) {
+  owner.textContent = text;
 }
+
+// fill picture link
+
+function fillPictureSource(owner, src) {
+  owner.src = src;
+}
+
+// add special class in depend on amount
+
+function renderAmount(amount) {
+  var myClass = '';
+
+  if (amount > 5) {
+    myClass = 'card--in-stock';
+  } else if (amount >= 1 && amount <= 5) {
+    myClass = 'card--little';
+  } else if (amount === 0) {
+    myClass = 'card--soon';
+  }
+
+  return myClass;
+}
+
+// add class and change text in depend on stars raiting
+
+function renderStars(owner, data) {
+  var rating = '';
+  var ratingText = ' звезды';
+
+  switch (data.rating.value) {
+    case 1:
+      rating = 'one';
+      ratingText = ' звезда';
+      break;
+    case 2:
+      rating = 'two';
+      break;
+    case 3:
+      rating = 'three';
+      break;
+    case 4:
+      rating = 'four';
+      break;
+    case 5:
+      rating = 'five';
+      ratingText = ' звезд';
+      break;
+  }
+
+  fillTextContent(owner, 'Рейтинг: ' + data.rating.value + ratingText);
+  return 'stars__rating--' + rating;
+}
+
+// change text in depend on sugar
+
+function renderIfSugar(data) {
+  var ifSugar = (data.nutrition_facts.sugar === true) ?
+    'Без сахара. ' + data.nutrition_facts.energy + ' ккал'
+    :
+    'Содержит сахар. ' + data.nutrition_facts.energy + ' ккал';
+
+  return ifSugar;
+}
+
+// create single card
+
+function createCatalogCard(data) {
+  var cardTemplate = document.querySelector('#card').cloneNode(true).content;
+  var card = cardTemplate.querySelector('.catalog__card');
+  var cardTitle = card.querySelector('.card__title');
+  var cardPictureSource = card.querySelector('.card__img');
+  var cardPrice = card.querySelector('.card__price').firstChild;
+  var cardWeight = card.querySelector('.card__weight');
+  var cardStars = card.querySelector('.stars__rating');
+  var cardStarsCount = card.querySelector('.star__count');
+  var cardCharacteristics = card.querySelector('.card__characteristic');
+  var cardComposition = card.querySelector('.card__composition-list');
+
+  card.classList.add(renderAmount(data.amount));
+  cardStars.classList.remove('stars__rating--five');
+  cardStars.classList.add(renderStars(cardStars, data));
+  fillTextContent(cardTitle, data.name);
+  fillPictureSource(cardPictureSource, data.picture);
+  fillTextContent(cardPrice, data.price);
+  fillTextContent(cardWeight, '/ ' + data.weight + ' Г');
+  fillTextContent(cardStarsCount, data.rating.number);
+  fillTextContent(cardCharacteristics, renderIfSugar(data));
+  fillTextContent(cardComposition, data.nutrition_facts.contents);
+
+  return card;
+}
+
+// modify DOM tree
+
+function fillCards(data, nest) {
+  var fragment = document.createDocumentFragment();
+  var cardsNest = nest;
+
+  for (var i = 0; i < data.length; i++) {
+    fragment.appendChild(createCatalogCard(data[i]));
+  }
+
+  cardsNest.appendChild(fragment);
+}
+
+fillCards(cards, catalogCardsNest);
+
+
+// Part 3. Generate cart products
+document.querySelector('.goods__card-empty').classList.add('visually-hidden');
+
+var cartCards = collectCards(3);
+var cartCardsNest = document.querySelector('.goods__cards');
+cartCardsNest.classList.remove('goods__cards--empty');
+
+function createCartCard(data) {
+  var cardTemplate = document.querySelector('#card-order').cloneNode(true).content;
+  var card = cardTemplate.querySelector('.card-order');
+  var cardTitle = card.querySelector('.card-order__title');
+  var cardPictureSource = card.querySelector('.card-order__img');
+  var cardPrice = card.querySelector('.card-order__price').firstChild;
+  var cardQuantity = card.querySelector('.card-order__count');
+
+  fillTextContent(cardTitle, data.name);
+  fillPictureSource(cardPictureSource, data.picture);
+  cardQuantity.value = getRandomInt(1, data.amount);
+  fillTextContent(cardPrice, data.price * cardQuantity.value + ' ₽');
+
+  return card;
+}
+
+function fillCartCards(data, nest) {
+  var fragment = document.createDocumentFragment();
+  var cardsNest = nest;
+
+  for (var i = 0; i < data.length; i++) {
+    fragment.appendChild(createCartCard(data[i]));
+  }
+
+  cardsNest.appendChild(fragment);
+}
+
+fillCartCards(cartCards, cartCardsNest);
